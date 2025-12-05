258808b36f0a823ffc152a049bb5eab3f1f0fcce
JIG ERA <jig111era@gmail.com>
2025-12-05 11:36:41 +0530

    ..

--- api/main.py
+++ api/main.py
@@ -6,7 +6,7 @@ import numpy as np
 app = FastAPI()
 
 # loading the saved model
-model = joblib.load("linear_regression_model.pkl")
+model = joblib.load("./model/linear_regression_model.pkl")
 
 @app.get("/")
 def home():
