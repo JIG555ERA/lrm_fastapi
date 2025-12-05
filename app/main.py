9fa47d81be0630619ebe4fa91929aa99872d97a0
JIG ERA <jig111era@gmail.com>
2025-12-05 11:58:31 +0530

    ammended procfile for railway deploymetb compatibility

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import numpy as np
 app = FastAPI()
 
 # loading the saved model
-model = joblib.load("./model/linear_regression_model.pkl")
+model = joblib.load("app/model/linear_regression_model.pkl")
 
 @app.get("/")
 def home():
